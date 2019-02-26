d0ff6fa7391e1c4162ed30d781896f6046d98b23
Bruno Kuasney <bruno.kuasney@gmail.com>
2019-02-25 21:11:35 -0300

    text header

--- face_recognition.py
+++ face_recognition.py
@@ -3,6 +3,11 @@ import logging
 import sys
 import os
 
+'''
+    The XML document it's a default document for opencv to identify frontal faces
+    There are many of XML documents to identify a lot of things, like cars, body humans, etc
+'''
+
 # configure logging
 logging.basicConfig(
     level=logging.DEBUG,
